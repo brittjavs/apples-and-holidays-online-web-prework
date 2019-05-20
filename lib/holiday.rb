c9e9461d0afee46944a167af93bcfe8eb3000b36
Brittany Javalera <brittjavalera@gmail.com>
2019-05-20 23:04:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
   holiday.each do |event, supplies|
-    puts "#{event.to_s.split("_").map { |word| word.capitalize }.join(" ") }: #{supplies.join(", ")}"
+    puts "  #{event.to_s.split("_").map { |word| word.capitalize }.join(" ") }: #{supplies.join(", ")}"
     
  end 
 end
